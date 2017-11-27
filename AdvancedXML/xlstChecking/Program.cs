893383c56e0ae38e128860beee5985afce13a30f
aleksand zareckij <zareckij.aleksandr@gmail.com>
2017-11-27 23:47:47 +0300

    first point

--- AdvancedXML/xlstChecking/Program.cs
+++ AdvancedXML/xlstChecking/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace xlstChecking
 {
@@ -10,6 +8,29 @@ namespace xlstChecking
     {
         static void Main(string[] args)
         {
+            XmlReaderSettings settings = new XmlReaderSettings();
+
+            settings.Schemas.Add("http://epam.com/learn/books", "BooksSchema.xsd");
+            settings.ValidationEventHandler +=
+                delegate (object sender, ValidationEventArgs e)
+                {
+                    Console.WriteLine("[{0}:{1}] {2}", e.Exception.LineNumber, e.Exception.LinePosition, e.Message);
+                };
+
+            settings.ValidationFlags = settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationType = ValidationType.Schema;
+
+            XmlReader reader1 = XmlReader.Create("books.xml", settings);
+
+            while (reader1.Read()) ;
+    
+            Console.WriteLine("Good file was checked");
+
+            XmlReader reader2 = XmlReader.Create("books_wrong.xml", settings);
+
+            while (reader2.Read());
+
+            Console.Read();
         }
     }
 }
